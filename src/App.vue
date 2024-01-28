d9c50097912beb703e6363fa23582d77c760f101
tcmmorker <3252588293@qq.com>
2024-01-29 01:06:47 +0800

    Second

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-input class="input" v-model="input" type="file" placeholder="Please input" />
+    <el-button class="button" type="primary">文件处理</el-button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+</style>
